31d0f02a407026de7a89237f12e7087d069c23d7
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2021-03-23 13:15:23 +0100

    Packaging configurations

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -4,7 +4,9 @@ var version = require('./package.json').version;
 // Custom webpack rules are generally the same for all webpack bundles, hence
 // stored in a separate local variable.
 var rules = [
-    { test: /\.css$/, use: ['style-loader', 'css-loader']}
+    { test: /\.css$/, use: ['style-loader', 'css-loader']},
+    //{ test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] }//,
+    //{ test: /\.json$/, loader: 'json-loader' }
 ]
 
 
